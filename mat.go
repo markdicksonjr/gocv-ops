cc602b8985215a9821b731c22ab0f75d7747fd64
Mark Dickson Jr <markdicksonjr@gmail.com>
2019-11-25 08:52:55 -0500

    Pulled in more resize ops

--- mat.go
+++ mat.go
@@ -3,6 +3,7 @@ package operations
 import (
 	"gocv.io/x/gocv"
 	"image"
+	"math"
 )
 
 func CopyMat(mat *gocv.Mat) *gocv.Mat {
@@ -11,8 +12,37 @@ func CopyMat(mat *gocv.Mat) *gocv.Mat {
 	return &newMat
 }
 
-func Resize(img gocv.Mat, targetWidth, targetHeight int) *gocv.Mat {
+func ResizeCopy(img gocv.Mat, targetWidth, targetHeight int) *gocv.Mat {
 	newMat := gocv.NewMat()
 	gocv.Resize(img, &newMat, image.Pt(targetWidth, targetHeight), 0, 0, gocv.InterpolationLanczos4)
 	return &newMat
+}
+
+func Resize(img *gocv.Mat, targetWidth, targetHeight int) {
+	gocv.Resize(*img, img, image.Pt(targetWidth, targetHeight), 0, 0, gocv.InterpolationLanczos4)
+}
+
+// ResizeWithoutStretching will chop the input image at the edges to the target aspect ratio, then scale it
+func ResizeWithoutStretching(mat *gocv.Mat, targetWidth, targetHeight int) {
+	aspectRatio := float64(targetWidth) / float64(targetHeight)
+	croppedHeight := int(math.Ceil(float64(mat.Cols()) / aspectRatio))
+	croppedWidth := int(math.Ceil(float64(croppedHeight) * aspectRatio))
+
+	x0 := mat.Cols()/2.0 - croppedWidth/2.0
+	y0 := mat.Rows()/2.0 - croppedHeight/2.0
+	newMat := mat.Region(image.Rect(
+		int(math.Max(float64(x0), 0)),
+		int(math.Max(float64(y0), 0)),
+		int(math.Min(float64(x0+croppedWidth), float64(mat.Cols()))),
+		int(math.Min(float64(y0+croppedHeight), float64(mat.Rows())))))
+	*mat = newMat
+
+	gocv.Resize(*mat, mat, image.Pt(targetWidth, targetHeight), 0, 0, gocv.InterpolationLanczos4)
+}
+
+// ResizeWithoutStretchingCopy will copy the input image before performing ResizeWithoutStretching
+func ResizeWithoutStretchingCopy(img gocv.Mat, targetWidth, targetHeight int) *gocv.Mat {
+	newMat := CopyMat(&img)
+	ResizeWithoutStretching(newMat, targetWidth, targetHeight)
+	return newMat
 }
